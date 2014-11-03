6437435fdd81b0407a2a8b93d303ed37f7686855
sarah1985 <sarahmleavy@gmail.com>
2014-11-02 22:23:24 -0500

    lesson 10 tasks 4-10

---
+++ picklecache.py
@@ -0,0 +1,64 @@
+#!usr/bin/env python
+# -*- coding: utf-8 -*-
+""" Task 04: Create the PickleCache Class"""
+
+import os
+import os.path
+import pickle
+
+
+class PickleCache(object):
+    """create pickle cache class"""
+
+    __file_object = None
+    __data = {}
+
+    def __init__(self, file_path='datastore.pkl'):
+        """constructor"""
+
+        self.__file_path = file_path
+
+    def set(self, key, value):
+        """public set method"""
+
+        self.__data[key] = value
+
+    def get(self, key):
+        """public get method"""
+
+        if key in self.__data:
+            return self.__data[key]
+        else:
+            print "Error: No value found for key: '{}'".format(key)
+
+    def delete(self, key):
+        """public delete method"""
+
+        if key in self.__data:
+            del self.__data[key]
+        else:
+            print "Error: No value found for key: '{}'".format(key)
+
+    def open(self):
+        """public open method"""
+
+        if os.path.exists(self.__file_path):
+            if os.path.getsize (self.__file_path) > 0:
+                temp = open(self.__file_path, 'r')
+                self.__data = pickle.load(temp)
+                temp.close()
+                open(self.__data, 'w')
+
+    def flush(self, reopen=True):
+        """pickle dump"""
+
+        pickle.dump(self.__data, self.__file_object)
+        self.__file_object.close()
+
+        if reopen:
+            self.open()
+
+    def close(self):
+        """close method"""
+
+        self.flush(reopen=False)
